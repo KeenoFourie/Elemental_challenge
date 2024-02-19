6431e5e097e909792d2841f9eaea58cb674fed5a
Keeno Fourie <keenofourie614@gmail.com>
2024-02-19 09:35:46 +0200

    edit

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,26 +1,33 @@
 $(document).ready(function () {
     let currentSlide = 0;
-    const slides = $('.slide');
+    const slides = $('.carousel-slide');
     let dots = $('.dot');
+    
     function showSlide(index) {
         currentSlide = (index + slides.length) % slides.length;
-        slides.css('transform', `translateX(${-100 * currentSlide}%)`);
-        dots.removeClass('active');
-        dots.eq(currentSlide).addClass('active');
+        slides.removeClass('active');
+        slides.eq(currentSlide).addClass('active');
+        dots.removeClass('dot-active');
+        dots.eq(currentSlide).addClass('dot-active');
     }
+    
     function changeSlide(offset) {
         showSlide(currentSlide + offset);
     }
-    $('.next.arrow').on('click', function () {
+    
+    $('.right-arrow').on('click', function () {
         changeSlide(1);
     });
+    
     $('.dots-container').on('click', '.dot', function () {
         const dotIndex = $(this).index();
         showSlide(dotIndex);
     });
+    
     for (let i = 0; i < slides.length; i++) {
         $('.dots-container').append('<div class="dot"></div>');
     }
+    
     dots = $('.dot');
     showSlide(currentSlide);
 });
