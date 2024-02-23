fba0da1da2d911c60d91c822f40678275cad7ec1
Keeno Fourie <keenofourie614@gmail.com>
2024-02-23 14:52:47 +0200

    update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,33 +1,33 @@
-$(document).ready(function () {
-    let currentSlide = 0;
-    const slides = $('.carousel-slide');
-    let dots = $('.dot');
+// $(document).ready(function () {
+//     let currentSlide = 0;
+//     const slides = $('.carousel-slide');
+//     let dots = $('.dot');
     
-    function showSlide(index) {
-        currentSlide = (index + slides.length) % slides.length;
-        slides.removeClass('active');
-        slides.eq(currentSlide).addClass('active');
-        dots.removeClass('dot-active');
-        dots.eq(currentSlide).addClass('dot-active');
-    }
+//     function showSlide(index) {
+//         currentSlide = (index + slides.length) % slides.length;
+//         slides.removeClass('active');
+//         slides.eq(currentSlide).addClass('active');
+//         dots.removeClass('dot-active');
+//         dots.eq(currentSlide).addClass('dot-active');
+//     }
     
-    function changeSlide(offset) {
-        showSlide(currentSlide + offset);
-    }
+//     function changeSlide(offset) {
+//         showSlide(currentSlide + offset);
+//     }
     
-    $('.right-arrow').on('click', function () {
-        changeSlide(1);
-    });
+//     $('.right-arrow').on('click', function () {
+//         changeSlide(1);
+//     });
     
-    $('.dots-container').on('click', '.dot', function () {
-        const dotIndex = $(this).index();
-        showSlide(dotIndex);
-    });
+//     $('.dots-container').on('click', '.dot', function () {
+//         const dotIndex = $(this).index();
+//         showSlide(dotIndex);
+//     });
     
-    for (let i = 0; i < slides.length; i++) {
-        $('.dots-container').append('<div class="dot"></div>');
-    }
+//     for (let i = 0; i < slides.length; i++) {
+//         $('.dots-container').append('<div class="dot"></div>');
+//     }
     
-    dots = $('.dot');
-    showSlide(currentSlide);
-});
+//     dots = $('.dot');
+//     showSlide(currentSlide);
+// });
